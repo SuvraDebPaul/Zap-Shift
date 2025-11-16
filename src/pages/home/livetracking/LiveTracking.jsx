ca16e1b3488b66daf74e58e885b75dcf2bd4e4ff
Suvra Deb Paul <suvra.deb.paul@gmail.com>
2025-11-16 19:17:31 +0600

    Merchant Section Done

--- src/pages/home/livetracking/LiveTracking.jsx
+++ src/pages/home/livetracking/LiveTracking.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import liveTrackingImg from "../../../assets/live-tracking.png";
 import liveTrackingImg2 from "../../../assets/safe-delivery.png";
+import merchantDesign from "../../../assets/Group-merchant.png";
+import merchantLocation from "../../../assets/location-merchant.png";
 
 const LiveTracking = () => {
   return (
@@ -52,6 +54,35 @@ const LiveTracking = () => {
               you with any questions, updates, or delivery concerns—anytime you
               need us.
             </p>
+          </div>
+        </div>
+      </div>
+      <div className="divider mb-20"></div>
+      <div className="bg-secondary rounded-2xl relative overflow-hidden text-base-100 mb-20">
+        <div className="absolute left-50 -top-70 -translate-left-50 w-full ">
+          <img src={merchantDesign} alt="" className="" />
+        </div>
+        <div className="flex justify-between gap-10 py-20 px-10">
+          <div className="basis-2/3">
+            <h3 className="text-4xl font-extrabold">
+              Merchant and Customer Satisfaction is Our First Priority
+            </h3>
+            <p className="my-5">
+              We offer the lowest delivery charge with the highest value along
+              with 100% safety of your product. Pathao courier delivers your
+              parcels in every corner of Bangladesh right on time.
+            </p>
+            <div className="space-x-4">
+              <button className="btn btn-primary text-secondary rounded-full">
+                Become a Merchant
+              </button>
+              <button className="btn btn-outline btn-primary rounded-full">
+                Earn with ZapShift Courier
+              </button>
+            </div>
+          </div>
+          <div className="basis-1/3">
+            <img src={merchantLocation} alt="" className="w-full" />
           </div>
         </div>
       </div>
